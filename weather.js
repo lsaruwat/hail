54614efac79e393fb7ec294e594e7fcb37935a9c
Logan Saruwatari <lsaruwatari@gmail.com>
2016-10-25 22:38:16 -0600

    added class for new api weatherunderground. Changed domupdater to be a method with this alias

--- weather.js
+++ weather.js
@@ -13,9 +13,16 @@ class Utilities{
 		textArea.innerHTML += "<h1>Weather in  " + respObj.name + "</h1>";
 		textArea.innerHTML += "<h2>Conditions: " + respObj.weather[0].description + "</h2>";
 		textArea.innerHTML += "<h2>Temp: " + Utilities.kelvinToFahren(respObj.main.temp) + "F " + Utilities.kelvinToCels(respObj.main.temp) + "C</h2>";
-		if(respObj.weather[0].description === "hail")textArea.innerHTML+="<h1>Hail: It's gonna HAIL!!!!!!!!!!!</h1>";
-		else textArea.innerHTML+="<h1>Hail: Looks like no!</h1>";
+		if(respObj.weather[0].description === "hail"){
+				textArea.innerHTML+="<h1>Hail: It's gonna HAIL!!!!!!!!!!!</h1>";
+				document.body.setAttribute("style", "background-color: #D74336;");
+		}
+		else{
+			document.body.setAttribute("style", "background-color: #0F9355;");
+			textArea.innerHTML+="<h1>Hail: Looks like no!</h1>";
+		} 
 	}
+
 }
 
 
@@ -37,12 +44,12 @@ class Weather{
 		return str;
 	}
 
-	apiCall(url){
+	apiCall(url, thisAlias=this){
 		fetch(url).then(function(response){
 		return response.json();
 		})
 		.then(function(respObj){
-			Utilities.domUpdater(respObj);
+			thisAlias.domUpdater(respObj);
 		});
 	}
 
@@ -52,4 +59,47 @@ class Weather{
 		this.url = "http://api.openweathermap.org/data/2.5/weather?lat="+this.lat+"&lon="+this.lon+"&appid=" + this.apiKey;
 		this.apiCall(this.url);
 	}
+
+	domUpdater(respObj){
+		let textArea = document.getElementById("text-area");
+		textArea.innerHTML += "<h1>Weather in  " + respObj.name + "</h1>";
+		textArea.innerHTML += "<h2>Conditions: " + respObj.weather[0].description + "</h2>";
+		textArea.innerHTML += "<h2>Temp: " + Utilities.kelvinToFahren(respObj.main.temp) + "F " + Utilities.kelvinToCels(respObj.main.temp) + "C</h2>";
+		if(respObj.weather[0].description === "hail"){
+				textArea.innerHTML+="<h1>Hail: It's gonna HAIL!!!!!!!!!!!</h1>";
+				document.body.setAttribute("style", "background-color: #D74336;");
+		}
+		else{
+			document.body.setAttribute("style", "background-color: #0F9355;");
+			textArea.innerHTML+="<h1>Hail: Looks like no!</h1>";
+		} 
+	}
+}
+
+
+class WeatherUnderground extends Weather{
+
+	constructor(apiKey){
+		super(apiKey);
+
+	}
+
+	getHailByCity(state,city){
+		this.state = state;
+		this.city = city;
+		this.url = "http://api.wunderground.com/api/" + this.apiKey + "/conditions/q/" + this.state + "/" + this.city + ".json";
+		this.apiCall(this.url);		
+	}
+
+	domUpdater(respObj){
+		let response = respObj.current_observation;
+		console.log(response);
+		let textArea = document.getElementById("text-area");
+		textArea.innerHTML += "<h1>Weather in  " + response.display_location.city + "</h1>";
+		textArea.innerHTML += "<h2>Conditions: " + response.weather + "</h2>";
+		textArea.innerHTML += "<h2>Temp: " + response.temp_f + "F " + response.temp_c + "C</h2>";
+		textArea.innerHTML += "<h2>" + response.observation_time + "</h2>";
+		textArea.innerHTML+="<img src=" + response.icon_url + " />";
+	}
+
 }
